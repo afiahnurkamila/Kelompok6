0f0399d795acc950f12b561fb703d190b8ac7733
Magfira_Isdayanti <firaisdayanti@gmail.com>
2017-01-10 09:45:16 +0800

    no message

--- Project_Kel6/FormLogin.vb
+++ Project_Kel6/FormLogin.vb
@@ -22,4 +22,8 @@
             TextBoxPassword.Clear()
         End If
     End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker1.ValueChanged
+
+    End Sub
 End Class
